f03ac62e0598408ebf08f548f01e04d43845d1e7
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-03 15:42:10 -0500

    troubleshooting deployment failure with skills pages loading

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,18 @@
 import Certifications from '../components/Certifications/Certifications.jsx';
 import Mission from '../components/Mission/Mission.jsx';
+/*
 import SoftSkills from '../components/SoftSkills/softSkills.jsx';
 import TechnicalSkills from '../components/TechnicalSkills/TechnicalSkills.jsx';
-
+*/
 const About = () => {
   return (
     <div className="page">
       <Certifications />
       <Mission />
-      <SoftSkills />
-      <TechnicalSkills />
+      {/*
+        <SoftSkills />
+        <TechnicalSkills />
+      */}
     </div>
   );
 };
