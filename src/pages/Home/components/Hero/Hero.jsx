bc2e70e6e8e87a3be19c348a780c650c8a3666b4
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-04 08:24:36 -0500

    edited hero 15

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -8,16 +8,15 @@ import HeroImg from '../../../../assets/logo.png';
 const Hero = () => {
   return (
     <div id="hero-container" className="container">
+      <img src={HeroImg} alt="An image of James." id="heroImg" />
       <h3>My name is James Huff.</h3>
-      <div id="hero-center">
-        <img src={HeroImg} alt="An image of James." id="heroImg" />
-        <p>
-          I am inclined to prioritize functionality and simplicity, as they
-          offer a more straightforward and effective approach compared to the
-          unnecessary complications of complexity.
-        </p>
-        <CallToAction />
-      </div>
+
+      <p>
+        I am inclined to prioritize functionality and simplicity, as they offer
+        a more straightforward and effective approach compared to the
+        unnecessary complications of complexity.
+      </p>
+      <CallToAction />
     </div>
   );
 };
