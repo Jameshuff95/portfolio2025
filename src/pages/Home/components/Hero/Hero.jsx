834b641bcedc0cc17cafb34ce2fa1f75b8c2a90b
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-04 07:24:40 -0500

    edited hero 7

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -11,13 +11,13 @@ const Hero = () => {
       <h2>My name is James Huff.</h2>
       <div id="hero-center">
         <img src={HeroImg} alt="An image of James." id="heroImg" />
+        <p>
+          I am inclined to prioritize functionality and simplicity, as they
+          offer a more straightforward and effective approach compared to the
+          unnecessary complications of complexity.
+        </p>
         <CallToAction />
       </div>
-      <p>
-        I am inclined to prioritize functionality and simplicity, as they offer
-        a more straightforward and effective approach compared to the
-        unnecessary complications of complexity.
-      </p>
     </div>
   );
 };
