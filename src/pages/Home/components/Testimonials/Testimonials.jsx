9cef8baa93b3a9dceaba7575afa2cbed334ba1c8
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-04 11:17:07 -0500

    editedt testimonials 1

--- src/pages/Home/components/Testimonials/Testimonials.jsx
+++ src/pages/Home/components/Testimonials/Testimonials.jsx
@@ -39,27 +39,27 @@ const Testimonials = () => {
   };
 
   return (
-    <div className="container">
+    <div id="testimonials-container" className="container">
       <h3>Testimonials</h3>
-      <div id="testimonial-carousel">
-        <i className="bi bi-arrow-left" onClick={prevTestimonial} />
-        <div className="testimonial">
-          <h3 className="author"></h3>
-          <div className="message">
-            <p className="message-top">
-              " {testimonials[currentTestimonial].message} "
+
+      <div className="testimonial">
+        <h3 className="author"></h3>
+        <div className="message">
+          <p className="message-top">
+            " {testimonials[currentTestimonial].message} "
+          </p>
+          <div className="message-bottom">
+            <p className="testimonial-author">
+              {testimonials[currentTestimonial].author}
             </p>
-            <div className="message-bottom">
-              <p className="testimonial-author">
-                {testimonials[currentTestimonial].author}
-              </p>
-              <p className="testimonial-date">
-                {testimonials[currentTestimonial].date}
-              </p>
-            </div>
+            <p className="testimonial-date">
+              {testimonials[currentTestimonial].date}
+            </p>
           </div>
         </div>
-
+      </div>
+      <div id="arrow-container">
+        <i className="bi bi-arrow-left" onClick={prevTestimonial} />
         <i className="bi bi-arrow-right" onClick={nextTestimonial} />
       </div>
     </div>
