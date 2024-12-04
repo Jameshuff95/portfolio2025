609122e3fd56a43ca9f141e674343237272e5661
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-04 10:25:02 -0500

    edited featured projects 1

--- src/pages/Home/components/FeaturedWork/FeaturedWork.jsx
+++ src/pages/Home/components/FeaturedWork/FeaturedWork.jsx
@@ -1,26 +1,28 @@
 import { useState } from 'react';
 import './FeaturedWork.css';
 
+import Img from '../../../../assets/logo.png';
+
 const FeaturedWork = () => {
   const featuredProjects = [
     {
       title: 'Project 1',
       technology: 'Tech 1',
-      img: '#',
+      img: Img,
       imgAlt: 'image alt 1',
       description: 'Description 1',
     },
     {
       title: 'Project 2',
       technology: 'Tech 2',
-      img: '#',
+      img: Img,
       imgAlt: 'image alt 2',
       description: 'Description 2',
     },
     {
       title: 'Project 3',
       technology: 'Tech 3',
-      img: '#',
+      img: Img,
       imgAlt: 'image alt 3',
       description: 'Description 3',
     },
@@ -42,10 +44,9 @@ const FeaturedWork = () => {
   };
 
   return (
-    <div className="container">
+    <div id="featured-work-container" className="container">
       <h3>Featured Work</h3>
       <div id="featured-projects-carousel">
-        <i className="bi bi-arrow-left" onClick={previousProject} />
         <div className="featuredProject" key={currentProject}>
           <img
             src={featuredProjects[currentProject].img}
@@ -58,7 +59,18 @@ const FeaturedWork = () => {
             {featuredProjects[currentProject].description}
           </p>
         </div>
-        <i className="bi bi-arrow-right" onClick={nextProject} />
+        <div id="arrow-container">
+          <i
+            id="left-arrow"
+            className="bi bi-arrow-left"
+            onClick={previousProject}
+          />
+          <i
+            id="right-arrow"
+            className="bi bi-arrow-right"
+            onClick={nextProject}
+          />
+        </div>
       </div>
     </div>
   );
