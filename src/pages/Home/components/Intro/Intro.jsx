b8d074cee2ae675c0de5642f1ddbaeed32b220bf
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-04 21:07:04 -0500

    edited intro again 1

--- src/pages/Home/components/Intro/Intro.jsx
+++ src/pages/Home/components/Intro/Intro.jsx
@@ -9,16 +9,12 @@ const Intro = () => {
       </h3>
       
       <p>
-        Hello! I'm James, a passionate self-taught junior web developer with a strong focus on the MERN stack (MongoDB, Express.js, React, Node.js). 
+        Hello! I'm James, a passionate self-taught web developer with a strong focus on the MERN stack (MongoDB, Express.js, React, Node.js). 
       </p>
 
       <p>
         My journey into the world of web development started with a thirst for knowledge and an insatiable curiosity. 
         Through rigorous self-study and hands-on practice, I have honed my skills in building dynamic, responsive web applications.
-      </p>
-
-      <p>
-        While my formal education includes some college coursework, my true learning has come from diving into real-world projects, collaborating with fellow developers, and continually pushing the boundaries of my capabilities. 
       </p>
       
       <p>
