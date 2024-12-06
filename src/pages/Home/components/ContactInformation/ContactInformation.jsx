d0abf5dae024a631ab1fb4115ec6669681a9879a
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-05 22:58:35 -0500

    edited styling

--- src/pages/Home/components/ContactInformation/ContactInformation.jsx
+++ src/pages/Home/components/ContactInformation/ContactInformation.jsx
@@ -1,42 +1,52 @@
 import { Link } from 'react-router-dom';
 import './ContactInformation.css';
+import '../../../../globalComponents/Footer/Footer.css';
 
 const ContactInformation = () => {
   return (
     <div id="social-links-container" className="container">
       <h3> Contact Me</h3>
       <div id="social-links">
-        <Link to="#">
-          <i className="bi bi-github" />
+        <Link to="#" className="contact-social">
+          <i id="github" className="bi bi-github" />
+          <legend>Github</legend>
         </Link>
 
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="linkedin" className="bi bi-linkedin" />
+          <legend>Linkedin</legend>
         </Link>
 
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="facebook" className="bi bi-facebook" />
+          <legend>Facebook</legend>
         </Link>
 
-        <Link to="#">
-          <i id="x" className="bi bi-twitter-x" />
+        <Link to="#" className="contact-social">
+          <i id="x2" className="bi bi-twitter-x" />
+          <legend>X</legend>
         </Link>
 
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="instagram" className="bi bi-instagram" />
+          <legend>Instagram</legend>
         </Link>
 
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="reddit" className="bi bi-reddit" />
+          <legend>Reddit</legend>
         </Link>
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="dribbble" className="bi bi-dribbble" />
+          <legend>Dribbble</legend>
         </Link>
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="youtube" className="bi bi-youtube" />
+          <legend>Youtube</legend>
         </Link>
-        <Link to="#">
+        <Link to="#" className="contact-social">
           <i id="stack-overflow" className="bi bi-stack-overflow" />
+          <legend>Stack Overflow</legend>
         </Link>
       </div>
     </div>
