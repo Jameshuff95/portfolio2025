f26f216fb5a2df2cb1b65764d7330ee1f4cc62af
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-08 07:40:59 -0500

    added project img test images

--- src/pages/Home/components/FeaturedWork/FeaturedWork.jsx
+++ src/pages/Home/components/FeaturedWork/FeaturedWork.jsx
@@ -1,31 +1,33 @@
-
 import { useState } from 'react';
 import { Carousel, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './FeaturedWork.css';
 
 import Img from '../../../../assets/logo.png';
+import FeaturedImg1 from '../../../../assets/logo-1.png';
+import FeaturedImg2 from '../../../../assets/logo-2.png';
+import FeaturedImg3 from '../../../../assets/logo-3.png';
 
 const FeaturedWork = () => {
   const featuredProjects = [
     {
       title: 'Project 1',
       technology: 'Tech 1',
-      img: Img,
+      img: FeaturedImg1,
       imgAlt: 'image alt 1',
       description: 'Description 1',
     },
     {
       title: 'Project 2',
       technology: 'Tech 2',
-      img: Img,
+      img: FeaturedImg2,
       imgAlt: 'image alt 2',
       description: 'Description 2',
     },
     {
       title: 'Project 3',
       technology: 'Tech 3',
-      img: Img,
+      img: FeaturedImg3,
       imgAlt: 'image alt 3',
       description: 'Description 3',
     },
@@ -35,22 +37,15 @@ const FeaturedWork = () => {
 
   return (
     <div id="featured-work-container" className="container">
-      <h3 className="container-title">
-        Featured Work
-      </h3>
-      <Carousel 
-        activeIndex={currentProject} 
-        onSelect={(selectedIndex) => 
-          setCurrentProject(selectedIndex)
-        }>
+      <h3 className="container-title">Featured Work</h3>
+      <Carousel
+        activeIndex={currentProject}
+        onSelect={(selectedIndex) => setCurrentProject(selectedIndex)}
+      >
         {featuredProjects.map((project, index) => (
           <Carousel.Item key={index}>
             <Card className="project-card text-center border-0">
-              <Card.Img 
-                variant="top" 
-                src={project.img} 
-                alt={project.imgAlt} 
-              />
+              <Card.Img variant="top" src={project.img} alt={project.imgAlt} />
               <Card.Body className="featured-item-body border-0">
                 <Card.Title className="featured-title border-0">
                   {project.title}
