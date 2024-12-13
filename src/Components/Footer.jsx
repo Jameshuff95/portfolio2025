6474516f5eed4db866e75f544b009c0591bf17c0
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-12 22:56:31 -0500

    simplified footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,32 +7,7 @@ import '../index.css';
 const Footer = () => {
   return (
     <Container id="footer">
-      <Row id="footer-top">
-        <Row className="footer-top-section">
-          <i className="bi bi-envelope" />
-          <Card.Text className="card-text-alt">
-            jameshuffdev@gmail.com
-          </Card.Text>
-        </Row>
-        <Row className="footer-top-section">
-          <i className="bi bi-phone" />
-          <Card.Text className="card-text-alt">+1-859-377-0700</Card.Text>
-        </Row>
-      </Row>
-
-      <hr />
-      <Col>
-        <Row>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/resume">Resumé</Link>
-          <Link to="/services">Services</Link>
-        </Row>
-      </Col>
-      <hr />
-      <Card.Text className="card-text-alt text-center">
+      <Card.Text className="card-text-alt">
         &copy; 2024 James Huff. All rights reserved.
       </Card.Text>
     </Container>
