8a434b6b1da96b13b88e07f8e9e2b0274e99941f
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-16 08:31:40 -0500

    edited hero size

--- src/Components/HeroSmall.jsx
+++ src/Components/HeroSmall.jsx
@@ -1,16 +1,14 @@
-import { Card, Container, Col } from 'react-bootstrap';
+import { Card, Container, Col, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import MeImg from '../assets/me.png';
 
 const HeroSmall = () => {
   return (
-    <Container
-      style={{ padding: '2%', border: '1px solid red', height: '84vh' }}
-    >
+    <Container style={{ padding: '2%', height: '84vh' }}>
       <Col
-        className=" d-flex flex-column justify-content-around "
-        style={{ border: '1px solid gold', height: '100%' }}
+        className=" d-flex flex-column justify-content-evenly align-items-center"
+        style={{ height: '100%' }}
       >
         {/*prettier-ignore */}
         <Card.Img src={MeImg} alt="An image of James." className="img-fluid"
@@ -19,12 +17,19 @@ const HeroSmall = () => {
               backgroundColor: 'var(--nav-bg)',
               border: '1px solid var(--nav-bg)',
               boxShadow: 'inset 0 0 1rem var(--card-text)',
-              width: '65%',
+              width: '75%',
               margin: '0 auto',
             }}
         />
-        <Card.Title>My name is James Huff.</Card.Title>
-        <Card.Text>
+
+        <Row>
+          <Card.Subtitle style={{ fontSize: '1.5rem' }}>
+            My name is
+          </Card.Subtitle>
+          <Card.Title style={{ fontSize: '2.75rem' }}>James Huff</Card.Title>
+        </Row>
+
+        <Card.Text style={{ fontSize: '1.5rem' }}>
           I am inclined to prioritize functionality and simplicity, as they
           offer a more straightforward and effective approach compared to the
           unnecessary complications of complexity.
