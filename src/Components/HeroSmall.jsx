3fcce7abafabf7577a615beaba17f4d26b4deabc
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-16 07:30:31 -0500

    edited hero size

--- src/Components/HeroSmall.jsx
+++ src/Components/HeroSmall.jsx
@@ -6,7 +6,7 @@ import MeImg from '../assets/me.png';
 const HeroSmall = () => {
   return (
     <Container
-      style={{ padding: '2%', border: '1px solid red', height: '90vh' }}
+      style={{ padding: '2%', border: '1px solid red', height: '88vh' }}
     >
       <Col
         className=" d-flex flex-column justify-content-center "
