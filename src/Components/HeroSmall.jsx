c73b941bb165d5bce7aa0fb105c8896c08ddf1e1
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-16 22:39:31 -0500

    edited heroIntro

--- src/Components/HeroSmall.jsx
+++ src/Components/HeroSmall.jsx
@@ -9,73 +9,59 @@ const HeroSmall = () => {
       style={{
         overflow: 'visible',
         padding: '2%',
-        height: '84vh',
-        overflow: 'visible',
+        height: '85vh',
         margin: '1rem auto',
       }}
     >
-      <Col
-        style={{
-          height: '100%',
-          border: '1px solid red',
-        }}
-      >
-        <Row
+      <Col style={{ height: '100%', overflow: 'visible' }}>
+        <Col
+          xs={8}
           style={{
             margin: 'auto',
-            border: '1px solid red',
-            height: '25%',
+            overflow: 'visible',
+            margin: '0 auto',
           }}
         >
-          <Card.Subtitle style={{ fontSize: '1.5rem' }}>
-            My name is
-          </Card.Subtitle>
-          <Card.Title style={{ fontSize: '2.75rem' }}>James Huff</Card.Title>
-        </Row>
-        <Col
-          className="d-flex flex-column justify-space-center"
-          style={{ border: '1px solid green', height: '75%' }}
+          <Card.Img
+            src={MeImg}
+            alt="An image of James."
+            className="img-fluid"
+            style={{
+              borderRadius: '50%',
+              backgroundColor: 'var(--nav-bg)',
+              border: '1px solid var(--nav-bg)',
+              boxShadow: 'inset 0 0 1rem var(--card-text)',
+              margin: '0 auto',
+            }}
+          />
+        </Col>
+
+        <Row
+          className="d-flex justify-content-center align-items-center"
+          style={{
+            overflow: 'visible',
+            margin: '0',
+            padding: '0',
+            height: '60%',
+          }}
         >
-          <Col
-            xs={8}
+          <Row
             style={{
               margin: 'auto',
-              overflow: 'visible',
-              margin: '0 auto',
-              border: '1px solid blue',
             }}
           >
-            <Card.Img
-              src={MeImg}
-              alt="An image of James."
-              className="img-fluid"
-              style={{
-                borderRadius: '50%',
-                backgroundColor: 'var(--nav-bg)',
-                border: '1px solid var(--nav-bg)',
-                boxShadow: 'inset 0 0 1rem var(--card-text)',
-                margin: '0 auto',
-              }}
-            />
-          </Col>
-
-          <Row
-            className="d-flex justify-content-center align-items-center"
-            style={{
-              overflow: 'visible',
-              margin: '0',
-              padding: '0',
-              border: '1px solid blue',
-              height: '100%',
-              fontSize: '1.25rem',
-            }}
-          >
-            <Card.Text>
-              I am inclined to prioritize functionality and simplicity, as they
-              offer a more straightforward and effective approach.
-            </Card.Text>
+            <Card.Subtitle style={{ margin: '0', padding: '0' }}>
+              My name is
+            </Card.Subtitle>
+            <Card.Title style={{ margin: '0', padding: '0' }}>
+              James Huff
+            </Card.Title>
           </Row>
-        </Col>
+          <Card.Text>
+            I am inclined to prioritize functionality and simplicity, as they
+            offer a more straightforward and effective approach.
+          </Card.Text>
+        </Row>
       </Col>
     </Container>
   );
