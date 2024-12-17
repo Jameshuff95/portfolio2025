b7ae6171a6ad9aa8a100dcc3de65d8d4d60c8d88
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-16 23:34:18 -0500

    edited hero

--- src/Components/HeroSmall.jsx
+++ src/Components/HeroSmall.jsx
@@ -13,55 +13,44 @@ const HeroSmall = () => {
         margin: '1rem auto',
       }}
     >
-      <Col style={{ height: '100%', overflow: 'visible' }}>
-        <Col
-          xs={8}
+      <Col
+        xs={12}
+        style={{
+          margin: 'auto',
+          overflow: 'visible',
+          margin: '0 auto',
+        }}
+      >
+        <Card.Img
+          src={MeImg}
+          alt="An image of James."
+          className="img-fluid"
           style={{
-            margin: 'auto',
-            overflow: 'visible',
+            borderRadius: '50%',
+            backgroundColor: 'var(--nav-bg)',
+            border: '1px solid var(--nav-bg)',
+            boxShadow: 'inset 0 0 1rem var(--card-text)',
             margin: '0 auto',
           }}
-        >
-          <Card.Img
-            src={MeImg}
-            alt="An image of James."
-            className="img-fluid"
-            style={{
-              borderRadius: '50%',
-              backgroundColor: 'var(--nav-bg)',
-              border: '1px solid var(--nav-bg)',
-              boxShadow: 'inset 0 0 1rem var(--card-text)',
-              margin: '0 auto',
-            }}
-          />
-        </Col>
-
-        <Row
-          className="d-flex justify-content-center align-items-center"
-          style={{
-            overflow: 'visible',
-            margin: '0',
-            padding: '0',
-            height: '60%',
-          }}
-        >
-          <Row
-            style={{
-              margin: 'auto',
-            }}
-          >
-            <Card.Subtitle style={{ margin: '0', padding: '0' }}>
-              My name is
-            </Card.Subtitle>
-            <Card.Title style={{ margin: '0', padding: '0' }}>
-              James Huff
-            </Card.Title>
-          </Row>
-          <Card.Text>
-            I am inclined to prioritize functionality and simplicity, as they
-            offer a more straightforward and effective approach.
-          </Card.Text>
+        />
+      </Col>
+      <Col
+        className="d-flex flex-column justify-content-evenly"
+        style={{ height: '48%' }}
+      >
+        <Row>
+          <Card.Subtitle style={{ margin: '0', padding: '0' }}>
+            My name is
+          </Card.Subtitle>
+          <Card.Title style={{ margin: '0', padding: '0' }}>
+            James Huff
+          </Card.Title>
         </Row>
+
+        <Card.Text>
+          I am inclined to prioritize functionality and simplicity, as they
+          offer a more straightforward and effective approach.
+        </Card.Text>
       </Col>
     </Container>
   );
