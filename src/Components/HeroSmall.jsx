97487e2cab75f5baa8eb0003277084b37556fc4b
Jameshuff95 <126839048+Jameshuff95@users.noreply.github.com>
2024-12-16 22:22:54 -0500

    edited heroIntro

--- src/Components/HeroSmall.jsx
+++ src/Components/HeroSmall.jsx
@@ -9,54 +9,73 @@ const HeroSmall = () => {
       style={{
         overflow: 'visible',
         padding: '2%',
-        height: '100%',
-        border: '1px solid red',
+        height: '84vh',
         overflow: 'visible',
+        margin: '1rem auto',
       }}
     >
       <Col
-        className="d-flex flex-column justify-content-evenly"
         style={{
           height: '100%',
-          overflow: 'visible',
+          border: '1px solid red',
         }}
       >
-        <Col xs={10} style={{ margin: 'auto', overflow: 'visible' }}>
-          {/*prettier-ignore */}
-          <Card.Img src={MeImg} alt="An image of James." className="img-fluid"
-            style={{
-              borderRadius: '50%',
-              backgroundColor: 'var(--nav-bg)',
-              border: '1px solid var(--nav-bg)',
-              boxShadow: 'inset 0 0 1rem var(--card-text)',
-              margin: '0 auto',
-            }}
-        />
-        </Col>
-
         <Row
           style={{
-            overflow: 'visible',
-            margin: '0',
+            margin: 'auto',
+            border: '1px solid red',
+            height: '25%',
           }}
         >
-          <Col style={{ border: '1px solid gold', overflow: 'visible' }}>
-            <Row>
-              <Card.Subtitle style={{ fontSize: '1.5rem' }}>
-                My name is
-              </Card.Subtitle>
-              <Card.Title style={{ fontSize: '2.75rem' }}>
-                James Huff
-              </Card.Title>
-            </Row>
-            <Row>
-              <Card.Text style={{ fontSize: '1.5rem' }}>
-                I am inclined to prioritize functionality and simplicity, as
-                they offer a more straightforward and effective approach.
-              </Card.Text>
-            </Row>
-          </Col>
+          <Card.Subtitle style={{ fontSize: '1.5rem' }}>
+            My name is
+          </Card.Subtitle>
+          <Card.Title style={{ fontSize: '2.75rem' }}>James Huff</Card.Title>
         </Row>
+        <Col
+          className="d-flex flex-column justify-space-center"
+          style={{ border: '1px solid green', height: '75%' }}
+        >
+          <Col
+            xs={8}
+            style={{
+              margin: 'auto',
+              overflow: 'visible',
+              margin: '0 auto',
+              border: '1px solid blue',
+            }}
+          >
+            <Card.Img
+              src={MeImg}
+              alt="An image of James."
+              className="img-fluid"
+              style={{
+                borderRadius: '50%',
+                backgroundColor: 'var(--nav-bg)',
+                border: '1px solid var(--nav-bg)',
+                boxShadow: 'inset 0 0 1rem var(--card-text)',
+                margin: '0 auto',
+              }}
+            />
+          </Col>
+
+          <Row
+            className="d-flex justify-content-center align-items-center"
+            style={{
+              overflow: 'visible',
+              margin: '0',
+              padding: '0',
+              border: '1px solid blue',
+              height: '100%',
+              fontSize: '1.25rem',
+            }}
+          >
+            <Card.Text>
+              I am inclined to prioritize functionality and simplicity, as they
+              offer a more straightforward and effective approach.
+            </Card.Text>
+          </Row>
+        </Col>
       </Col>
     </Container>
   );
